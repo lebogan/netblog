bdb958b3896f5b9eb1715b1754d5c6a84a97c810
Lewis E. Bogan <lewis.bogan@comcast.net>
2019-05-26 09:42:42 -0700

    remove version file

--- src/netblog.cr
+++ src/netblog.cr
@@ -122,41 +122,6 @@ post "/log/:id/edit" do |env|
   end
 end
 
-# get "/maintenance" do |env|
-#  title = "Maintenance"
-#  my_renderer "maintenance"
-# end
-
-# post "/backup" do |env|
-#  if env.params.body["prune"] == "true"
-#    number_old_files = prune_files
-#    env.flash["success"] = "Backups successful. Deleted #{number_old_files} old backup files." if run_backup
-#  else
-#    env.flash["success"] = "Backups successful!" if run_backup
-#  end
-#  env.redirect "/"
-# end
-
-# post "/restore" do |env|
-#  status, result = run_restore(env)
-#  if status == 0
-#    env.flash["success"] = "Restore successful!"
-#  else
-#    env.flash["warning"] = "Restore failed: #{result}"
-#  end
-#  env.redirect "/"
-# end
-
-# post "/integrity_check" do |env|
-#  status, result = integrity_check
-#  if status == 0 && result.includes?("ok")
-#    env.flash["success"] = "All's good in the neighborhood!"
-#  else
-#    env.flash["danger"] = "Check under the hood: #{result}"
-#  end
-#  env.redirect "/"
-# end
-
 get "/search" do
   title = "Search"
   my_renderer "search"
